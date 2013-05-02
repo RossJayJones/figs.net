0161437d17c46820f0b1ff3267105692c2ccc972
Ross Jones <ross@appworks.co.za>
2013-05-02 13:39:06 +0200

    Added support for web project

--- Trackmatic.Figs/JsonSettingsProvider.cs
+++ Trackmatic.Figs/JsonSettingsProvider.cs
@@ -18,6 +18,8 @@ namespace Trackmatic.Figs
             var settings = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(File.ReadAllText(_path));
             if (!settings.ContainsKey("configuration") || !settings["configuration"].ContainsKey("active"))
                 throw new KeyNotFoundException("An active configuration has not been supplied");
+            if (!settings.ContainsKey(settings["configuration"]["active"]))
+                throw new KeyNotFoundException(string.Format("Configured configuration {0} could not be found", settings["configuration"]["active"]));
             return settings[settings["configuration"]["active"]];
         }
     }
