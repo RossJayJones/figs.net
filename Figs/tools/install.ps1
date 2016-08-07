2cd7639c451306ed2ca852bad3940e2c14b778e2
Ross Jones <rossj@trackmatic.co.za>
2016-08-07 13:38:38 +0200

    add cleanup script for older versions

--- Figs/tools/install.ps1
+++ Figs/tools/install.ps1
@@ -1,5 +1,13 @@
 param($installPath, $toolsPath, $package, $project)
 	
+	# Clean up older version of figs
+	$proj = [xml](get-content $project.FullName)
+	$target = $proj.GetElementsByTagName("Target") |? { $_.Name -eq "AfterBuild" -and $_.GetElementsByTagName("Figs").Count -gt 0 }
+	if ($target) { $proj.ChildNodes[1].RemoveChild($target) }
+	$import = $proj.GetElementsByTagName("Import") |? { $_.Project.Contains("Figs") }
+	if($import) { $proj.ChildNodes[1].RemoveChild($import) }
+	$proj.save($project.FullName)
+	
 	# Need to load MSBuild assembly if it's not loaded yet.
 	add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
 	
